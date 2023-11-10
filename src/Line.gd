b08f25b071995c91f5ce89200feb09a84ddc7b49
LayduA <adrien.laydu@gmail.com>
2023-11-10 18:21:56 +0100

    Platform fuel (and re-fuel) implemented

--- src/Line.gd
+++ src/Line.gd
@@ -1,14 +1,14 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var line_speed = 200.0
-
+@export var line_speed = 2.5
+var velocity
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity.x = line_speed
+	velocity = Vector2(line_speed, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_and_slide()
+	self.position += velocity
 		
 func _on_screen_exited():
 	self.position.x = 0.0
--- src/Platform.gd
+++ src/Platform.gd
@@ -1,12 +1,14 @@
 class_name Platform
 extends StaticBody2D
  
-@onready var is_generating = false # Boolean to toggle (mainly to stop) a platform generation
+@onready var is_generating = false
+@onready var is_degenerating = false 
 var line
-
+var index
 
 func _ready():
-	line = get_parent().get_node("Line").get_node("Line_body")
+	line = get_parent().get_node("line/line_area")
+	
 	# Fixes the left side of the rectangles to the current position of the line
 	var Platform_view = Polygon2D.new()
 	Platform_view.set_polygon(PackedVector2Array([
@@ -24,10 +26,11 @@ func _ready():
 	]))
 	Platform_hitbox.set_name("Platform_hitbox")
 	self.set_collision_layer_value(4, true)
+	self.set_collision_layer_value(5, true)
 	Platform_view.set_name("Platform_view")
 	self.add_child(Platform_hitbox)
 	self.add_child(Platform_view)
-	line.get_node("Line_notifier").connect("screen_exited", stop_generating)
+	line.get_node("line_screennotifier").connect("screen_exited", stop_generating)
 	self.set_visible(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,6 +41,17 @@ func _process(_delta):
 		$Platform_view.polygon[3][0] = line.position.x
 		$Platform_hitbox.polygon[2][0] = line.position.x
 		$Platform_hitbox.polygon[3][0] = line.position.x
+		get_parent().use_fuel(0.15)
+	if is_degenerating:
+		$Platform_view.polygon[0][0] = line.position.x
+		$Platform_view.polygon[1][0] = line.position.x
+		$Platform_hitbox.polygon[0][0] = line.position.x
+		$Platform_hitbox.polygon[1][0] = line.position.x
+		if $Platform_view.polygon[0][0] >= $Platform_view.polygon[2][0]:
+			queue_free()
+		get_parent().refuel(0.15)
+	if Input.is_action_just_released("ui_delete_notes"):
+		is_degenerating = false
 
 func set_y_coordinate(y):
 	$Platform_view.polygon[0][1] = y
@@ -47,7 +61,7 @@ func set_y_coordinate(y):
 	$Platform_hitbox.polygon[0][1] = y
 	$Platform_hitbox.polygon[1][1] = y + 10
 	$Platform_hitbox.polygon[2][1] = y + 10
-	$Platform_hitbox.polygon[3][1] = y 
+	$Platform_hitbox.polygon[3][1] = y
 
 func stop_generating():
 	self.is_generating = false
